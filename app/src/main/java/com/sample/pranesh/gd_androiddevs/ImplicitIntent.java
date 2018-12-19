8b91a91dff07f0b5e0f706e1d886a2a184d2eeee
Pranesh <ppranesh5@gmail.com>
2018-12-19 21:18:03 +0530

    app debugging done

--- app/src/main/java/com/sample/pranesh/gd_androiddevs/ImplicitIntent.java
+++ app/src/main/java/com/sample/pranesh/gd_androiddevs/ImplicitIntent.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ImplicitIntent extends AppCompatActivity {
 
+    private static String TAG = "Implicit Intent - 5";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +31,47 @@ public class ImplicitIntent extends AppCompatActivity {
                 startActivity(int2);
                 break;
 
+
             case R.id.map :
                 Intent int3 = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:37.0902,95.7129"));
                 startActivity(int3);
                 break;
         }
+    }
+
+    public void nxtpgg(View view) {
+        Intent int1 = new Intent(ImplicitIntent.this,DebugCheck.class);
+        startActivity(int1);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"onResume");
     }
 }
